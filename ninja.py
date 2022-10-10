9995ea856a939fa98dfe3670c0d310b95f26fd7b
Jeffrey Frederick <doubleint@gmail.com>
2022-10-09 22:16:59 -0500

    Add files via upload

---
+++ ninja.py
@@ -0,0 +1,40 @@
+import requests
+import random
+ 
+proxies = { 
+	'http': 'http://45.79.90.143:44554', 
+	'http': 'http://45.79.142.211:3128', 
+    'http': 'http://47.74.136.452:2246',
+    'https': 'https://45.79.90.143:44554', 
+	'https': 'https://45.79.142.211:3128', 
+    'https': 'https://47.74.136.452:2246',
+} 
+user_agents = [ 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
+	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36', 
+	'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148', 
+	'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36',
+] 
+headers = { 
+	'authority': 'httpbin.org', 
+	'cache-control': 'max-age=0', 
+	'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"', 
+	'sec-ch-ua-mobile': '?0', 
+	'upgrade-insecure-requests': '1', 
+	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 
+	'sec-fetch-site': 'none', 
+	'sec-fetch-mode': 'navigate', 
+	'sec-fetch-user': '?1', 
+	'sec-fetch-dest': 'document', 
+	'accept-language': 'en-US,en;q=0.9', 
+} 
+cookies = ""
+
+random_proxy = random.choice(proxies)
+random_user_agent = random.choice(user_agents)
+random_header = random.choice(headers)
+
+response = requests.get('http://httpbin.org/headers', cookies=cookies, proxies=random_proxy, user_agents=random_user_agent, headers=random_header) 
+print(response)
